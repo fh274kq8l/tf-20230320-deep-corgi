8317eb00081bc1f64774f33236c7cc1ee7662dc8
A <github982457@protonmail.com>
2023-03-21 17:19:52 +0000

    RG names

--- terraform-aks-deploy/aks_cluster.tf
+++ terraform-aks-deploy/aks_cluster.tf
@@ -1,14 +1,14 @@
-# # Generate random resource group name
-# resource "random_pet" "rg_name" {
-#   prefix = var.resource_group_name_prefix
-# }
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
 
-# resource "azurerm_resource_group" "rg" {
-#   location = var.resource_group_location
-#   name     = random_pet.rg_name.id
-# }
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
 
-data "azurerm_resource_group" "rg" {
+data "azurerm_resource_group" "example" {
   name = var.resource_group_name
 }
 
