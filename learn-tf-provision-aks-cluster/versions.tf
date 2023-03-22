8c298ad4958a6db4fb59704985bbc313e5eae854
A <github982457@protonmail.com>
2023-03-22 17:29:09 +0000

    Removed duplicate tf block

--- learn-tf-provision-aks-cluster/versions.tf
+++ learn-tf-provision-aks-cluster/versions.tf
@@ -1,17 +1,17 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.66.0"
-    }
-  }
-  backend "azurerm" {
-    key      = "terraform.tfstate"
-    use_oidc = true
-  }
-  required_version = ">= 0.14"
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "2.66.0"
+#     }
+#   }
+#   backend "azurerm" {
+#     key      = "terraform.tfstate"
+#     use_oidc = true
+#   }
+#   required_version = ">= 0.14"
+# }
 
