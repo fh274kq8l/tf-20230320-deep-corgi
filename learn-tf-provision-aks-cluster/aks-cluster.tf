c492b6d731519d2f45efa4de0fdfabfb025279ae
A <github982457@protonmail.com>
2023-03-22 17:56:36 +0000

    Location

--- learn-tf-provision-aks-cluster/aks-cluster.tf
+++ learn-tf-provision-aks-cluster/aks-cluster.tf
@@ -5,6 +5,7 @@ resource "random_pet" "prefix" {}
 
 resource "azurerm_resource_group" "default" {
   name = var.resource_group_name
+  location = "West Europe"
 }
 
 # resource "azurerm_resource_group" "default" {
