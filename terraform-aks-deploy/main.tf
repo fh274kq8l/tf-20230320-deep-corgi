16cb82b25f95c61194a9dd3a32bcedf36e37ab1f
A <github982457@protonmail.com>
2023-03-22 10:50:29 +0000

    System-assigned

--- terraform-aks-deploy/main.tf
+++ terraform-aks-deploy/main.tf
@@ -1,33 +1,37 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.30.0"
     }
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
       version = "2.30.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
-    }    
+    }
   }
   backend "azurerm" {
-    key = "terraform.tfstate"
+    key      = "terraform.tfstate"
     use_oidc = true
   }
 }
 
 provider "azurerm" {
-  use_oidc = true
+  use_oidc                   = true
   skip_provider_registration = true
   features {}
-  subscription_id = data.azurerm_client_config.default.subscription_id
-  tenant_id       = data.azurerm_client_config.default.tenant_id  
+  # subscription_id = data.azurerm_client_config.default.subscription_id
+  # tenant_id       = data.azurerm_client_config.default.tenant_id  
 }
 
 provider "azuread" {
 }
 
 data "azurerm_client_config" "current" {}
+
+identity {
+  type = "SystemAssigned"
+}
